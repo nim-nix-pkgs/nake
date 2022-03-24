99d87c6a678a0122478ca53f70fa8cfb2bba8dd3
hugosenari <hugosenari@gmail.com>
2022-03-23 23:04:23 -0300

    feat(nimpkgs): remove lock

--- v1_2/flake.nix
+++ v1_2/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''make-like for Nim. Describe your builds as tasks!'';
+  description = ''make-like for Nimrod. Describe your builds as tasks!'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
